25ae927b6719b4e6a41aed88768ef4085371f467
juranja3 <juranja3@fit.cvut.cz>
2017-01-27 15:25:32 +0100

    ModelPool fixed after merge

--- src/model/ModelPool.m
+++ src/model/ModelPool.m
@@ -13,9 +13,22 @@ classdef ModelPool < Model
         transformCoordinates  % transform X-space
         stateVariables        % variables needed for sampling new points as CMA-ES do
         
+        
+        % GpModel specific fields
+        stdY                  % standard deviation of Y in training set, for normalizing output
+        options
+        hyp
+        meanFcn
+        covFcn
+        likFcn
+        infFcn
+        nErrors
+        trainLikelihood
+        
         % ModelPool specific properties
+        dimReduction
+        modelPoolOptions
         archive
-        modelOptions
         models
         modelsCount
         bestModel
@@ -23,12 +36,11 @@ classdef ModelPool < Model
         retrainPeriod
         historyLength
         xMean
-        trainLikelihood
     end
     
     methods (Access = public)
         function obj = ModelPool(modelOptions, xMean, archive)
-            obj.modelOptions = modelOptions;
+            obj.modelPoolOptions = modelOptions;
             obj.xMean = xMean;
             obj.archive = archive;
             obj.modelsCount = size(modelOptions.parameterSets,2);
@@ -38,17 +50,25 @@ classdef ModelPool < Model
             obj.dim       = size(xMean, 2);
             obj.shiftMean = zeros(1, obj.dim);
             obj.shiftY    = 0;
-            
+            obj.stdY  = 1;
             %create the models
             for i=1:obj.modelsCount
                 options = modelOptions.parameterSets(i);
                 obj.models(end+1) = ModelFactory.createModel('gp',options,xMean);
             end
-            obj.bestModel=0;
-            
             obj.historyLength = modelOptions.historyLength;
             obj.retrainPeriod = modelOptions.retrainPeriod;
             obj.predictionType = modelOptions.predictionType;
+            obj.bestModel=1;
+            
+            obj.options.normalizeY = defopts(modelOptions, 'normalizeY', true);
+            
+            % general model prediction options
+            obj.predictionType = defopts(modelOptions, 'predictionType', 'fValues');
+            obj.transformCoordinates = defopts(modelOptions, 'transformCoordinates', true);
+            obj.dimReduction = defopts(modelOptions, 'dimReduction', 1);
+            
+            obj = obj.copyVariablesFromBestModel();
         end
         
         function nData = getNTrainData(obj)
@@ -59,21 +79,24 @@ classdef ModelPool < Model
         end
         
         function obj = trainModel(obj, X, y, xMean, generation)
+            
             if (mod(generation,obj.retrainPeriod)==0)
                 generations=obj.trainGeneration+1:generation;
                 [X2,y2]=obj.archive.getDataFromGenerations(generations);
-                obj.trainMean = xMean;
-                obj.dataset.X = X;
-                obj.dataset.y = y;
+                
                 for i=1:obj.modelsCount
                     nTrainData = obj.models(i,1).getNTrainData();
+                    %Todo: rework, only 1 model needs to be trained,
+                    %models that were not trained in previous trainModel
+                    %calls should be trained in next call with more data
                     if (nTrainData>size(X,1))
                         warning('ModelPool.trainModel(): not enough data for model no. %d.',i);
                     else
                         %TODO:choose the data
-                        modelXData = X(1:nTrainData,:);
-                        modelYData = y(1:nTrainData,:);
-                        
+                        %modelXData = X(1:nTrainData,:);
+                        %modelYData = y(1:nTrainData,:);
+                        modelXData = X;
+                        modelYData = y;
                         circshift(obj.models(i),1,1);
                         if (size(obj.models(i))<obj.historyLength)
                             obj.models(i,end+1) = obj.models(i,1);
@@ -85,6 +108,10 @@ classdef ModelPool < Model
                 
                 obj.trainGeneration = generation;
                 obj.bestModel = obj.chooseBestModel();
+                obj = obj.copyVariablesFromBestModel();
+                obj.dataset.X = obj.models(obj.bestModel,1).dataset.X;
+                obj.dataset.y = obj.models(obj.bestModel,1).dataset.y;
+                
             end
         end
         
@@ -105,6 +132,23 @@ classdef ModelPool < Model
                 end
             end
             obj.bestModelsHistory(bestModelIndex) = obj.bestModelsHistory(bestModelIndex)+1;
+        end
+        
+        %used to simulate the behavior of the best GpModel
+        function obj = copyVariablesFromBestModel(obj)
+
+            obj.stdY = obj.models(obj.bestModel,1).stdY;
+            obj.options = obj.models(obj.bestModel,1).options;
+            obj.hyp = obj.models(obj.bestModel,1).hyp;
+            obj.meanFcn = obj.models(obj.bestModel,1).meanFcn;
+            obj.covFcn = obj.models(obj.bestModel,1).covFcn;
+            obj.likFcn = obj.models(obj.bestModel,1).likFcn;
+            obj.infFcn = obj.models(obj.bestModel,1).infFcn;
+            obj.nErrors = obj.models(obj.bestModel,1).nErrors;
+            obj.trainLikelihood = obj.models(obj.bestModel,1).trainLikelihood;
+            
+            obj.shiftY = obj.models(obj.bestModel,1).shiftY;
+            obj.trainMean = obj.models(obj.bestModel,1).trainMean;
         end
     end
     
