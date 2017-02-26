d1f834f337815081513fe73268f5af9c5661df10
juranja3 <juranja3@fit.cvut.cz>
2017-02-26 14:39:21 +0100

    Returning points that are in trainRange fixed

--- src/data/Archive.m
+++ src/data/Archive.m
@@ -123,6 +123,51 @@ classdef Archive < handle
       y = obj.y(indicesToReturn);
     end
     
+    function [X, y] = getClosestDataFromPointsInTrainRange(obj, n, xInput, trainRange, sigma, BD)
+      % returns union of 'n'-tuples of points which are closest to each
+      % of data points from the points in 'xInput'
+      % using (sigma*BD)-metric
+      % if (n == 0), all the available data are returned
+      nData = length(obj.y);
+      X = []; y = [];
+      
+      if (nData == 0)
+        return;
+      end
+      
+      if (nData > n)
+        % there are more data than 'n'
+        indicesToReturn = false(size(obj.y,1),1);
+        
+        % compute coordinates in the (sigma*BD)-basis
+        BDinv = inv(sigma*BD);
+        % for each point from xInput:
+        for i = 1:size(xInput,1)
+          xTransf = ( BDinv * (obj.X - repmat(xInput(i,:),nData,1))' )';
+          diff = sum(xTransf.^2, 2);
+          isInRange = diff < (trainRange ^ 2);
+          % take up to 'n' closest points from current point xInput(i,:)
+          [~, closest] = sort(diff);
+          closest((n+1):end) = [];
+          
+          % union these points with the previous points, if they are in
+          % trainRange
+          for j = 1:size(closest)
+            point = closest(j);
+            if (isInRange(point))
+              indicesToReturn(point) = true;
+            end
+          end
+        end
+      else
+        indicesToReturn = true(size(obj.y,1),1);
+      end
+      
+      % return the final points
+      X = obj.X(indicesToReturn,:);
+      y = obj.y(indicesToReturn);
+    end
+    
     function [X, y] = getClosestDataFromPopulation(obj, n, trainRange, population, sigma, BD)
       % returns points which are closest to each
       % of data points from the points in 'population'
@@ -152,12 +197,16 @@ classdef Archive < handle
         end
         % take up to 'n' closest points
         isInRange = diff < (trainRange ^ 2);
-        diff = diff(isInRange);
         [~, closest] = sort(diff);
         closest((n+1):end) = [];
+        % take only points that are in trainRange
+        for j = 1:size(closest)
+          point = closest(j);
+          if (isInRange(point))
+            indicesToReturn(point) = true;
+          end
+        end
         
-        % union these points with the previous points
-        indicesToReturn(closest) = true;
       else
         indicesToReturn = true(size(obj.y,1),1);
       end
@@ -180,9 +229,9 @@ classdef Archive < handle
         case 'clustering'
           [X, y] = obj.getDataNearPoint(trainsetSizeMax, xMean, trainRange, sigma, BD);
         case 'nearest'
-          [X, y] = obj.getClosestDataFromPoints(trainsetSizeMax, xMean, sigma, BD);
+          [X, y] = obj.getClosestDataFromPointsInTrainRange(trainsetSizeMax, xMean, trainRange, sigma, BD);
         case 'nearesttopopulation'
-          [X, y] = obj.getClosestDataFromPopulation(trainsetSizeMax, trainRange, population, sigma, BD);
+          [X, y] = obj.getClosestDataFromPopulation(trainsetSizeMax, population, trainRange, sigma, BD);
       end
     end
   end
