4044e34e5fc2db36c97c5061a90eef1f6330ff9e
juranja3 <juranja3@fit.cvut.cz>
2017-03-30 16:58:25 +0200

    Merge and GP_test_02_noisy.m update

--- exp/experiments/exp_GPtest_02_noisy.m
+++ exp/experiments/exp_GPtest_02_noisy.m
@@ -8,10 +8,8 @@ modelOptions.hyp.cov         = log([0.5; 2]);
 modelOptions.covBounds       = [ [-2;-2], [25;25] ];
 modelOptions.likBounds       = log([1e-6, 10]);
 
-% Full factorial design of the following parameters
 modelOptions.trainsetType    = { 'nearestToPopulation', 'nearest', 'clustering', 'allPoints' };
 modelOptions.trainRange      = { 1.0, 0.999 };
 modelOptions.trainsetSizeMax = { '5*dim', '10*dim', '15*dim', '20*dim' };
 modelOptions.meanFcn         = { 'meanConst', 'meanLinear' };
-modelOptions.covFcn          = { '{@covSEiso}', '{@covSEard}', ...
-                            '{@covMaterniso, 5}', '{@covMaterniso, 3}' };
+modelOptions.covFcn          = { '{@covSEiso}', '{@covSEard}', '{@covMaterniso, 5}', '{@covMaterniso, 3}' };
