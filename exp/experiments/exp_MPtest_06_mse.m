d34cc9aab8d3ec9c4d19f631104ef54b09c15baa
juranja3 <juranja3@fit.cvut.cz>
2017-04-16 14:05:36 +0200

    10D MP tests added

---
+++ exp/experiments/exp_MPtest_06_mse.m
@@ -0,0 +1,14 @@
+modelOptions.useShift        = false;
+modelOptions.predictionType  = 'sd2';
+modelOptions.normalizeY      = true;
+modelOptions.modelType = 'modelPool';
+opts.modelType = 'modelPool';
+
+modelOptions.parameterSets = {struct('parameterSets.covFcn', {'{@covSEard}', '{@covSEard}', '{@covSEard}', '{@covMaterniso, 3}', '{@covSEard}', '{@covMaterniso, 3}', '{@covMaterniso, 3}', '{@covMaterniso, 3}', '{@covMaterniso, 3}', '{@covMaterniso, 5}', '{@covMaterniso, 3}', '{@covMaterniso, 3}', '{@covMaterniso, 3}'},'parameterSets.trainsetType', {'nearest', 'nearest', 'allPoints', 'nearest', 'clustering', 'nearest', 'allPoints', 'allPoints', 'allPoints', 'allPoints', 'allPoints', 'clustering', 'nearestToPopulation'},'parameterSets.trainRange', {1, 1, 1, 1, 0.999, 1, 1, 1, 1, 1, 0.999, 0.999, 1},'parameterSets.trainsetSizeMax', {'20*dim', '15*dim', '5*dim', '5*dim', '20*dim', '5*dim', '5*dim', '10*dim', '20*dim', '20*dim', '20*dim', '15*dim', '15*dim'},'parameterSets.meanFcn', {'meanConst', 'meanConst', 'meanConst', 'meanConst', 'meanConst', 'meanLinear', 'meanConst', 'meanConst', 'meanConst', 'meanConst', 'meanConst', 'meanLinear', 'meanConst'},'parameterSets.trainAlgorithm', {'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon', 'fmincon'},'parameterSets.hyp', {struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147]), struct('lik', -4.605170, 'cov', [-0.693147, 0.693147])})};
+
+% Full factorial design of the following parameters                          
+modelOptions.bestModelSelection = { 'mse' };
+modelOptions.historyLength      = { 3, 5, 7 };
+modelOptions.minTrainedModelsPercentilForModelChoice = {0.25, 0.5};
+modelOptions.maxGenerationShiftForModelChoice = {0, 2};
+                          
